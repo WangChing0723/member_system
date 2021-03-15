5955047121711b9778345cc01170572f411b5f81
Cheng <jeff.cheng@volkswagen.com.tw>
2021-03-15 13:31:08 +0800

    delete import url_for

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask import request
 from flask import redirect
 from flask import session
-from flask import url_for
 from flask import render_template
 
 app = Flask(__name__)
